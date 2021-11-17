aec7d761222c40c2a523a23f7408bd75c66aa2b4
maximkhilko <94455286+maximkhilko@users.noreply.github.com>
2021-11-17 16:13:01 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,48 @@
+package com.learnup.lesson3;
+import java.util.Random;
+import java.util.Arrays;
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        int[] array = new int[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = new Random().nextInt();
+        }
+        System.out.println(Arrays.toString(array)); //исходный массив с рандомными значениями
+        System.out.println(arraySum(array));    //сумма массива
+
+        System.out.println((arraySum(array) % 2) == 0 ? "ЧЕТНО" : "НЕЧЕТНО"); //четность суммы массива
+         
+    }
+
+    public static int arraySum(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+
+
+}
+
+
+
+
+
+
+
+//вывести в консоль сумму и ЧЕТНО/НЕЧЕТНО
+
+
+
+
+
+
+
+
+
+
+
